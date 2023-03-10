34dd1137bc8108e05e916527b226191c4ee21884
maikeljh <mjh191003@gmail.com>
2023-03-11 01:58:42 +0700

    add TSP with DFS

--- src/app/dfs.cs
+++ src/app/dfs.cs
@@ -59,6 +59,62 @@ class DFS {
         }
     }
 
+    static void tspDFS(char[,] map, int x, int y, int xBefore, int yBefore, bool[,] isVisited, List<Tuple<int,int>> path, List<Tuple<int,int>> track, ref bool finish){
+        /*
+            The main TSP DFS recursive function
+            Param :
+                map : the given map
+                x : the current x coordinate
+                y : the current y coordinate
+                xBefore : the x coordinate before
+                yBefore : the y coordinate before
+                isVisited : states which coordinate has been visited or not
+                path : collects the path from last treasure found to starting point
+                track : collects temporary path to starting point, will be removed if the current track doesn't end on starting point
+        */
+
+        // Getting N
+        int size = map.GetLength(0);
+
+        // If map is not out of bounds
+        if(x >= 0 && x < size && y >= 0 && y < size && !finish){
+            // If map is already visited or blocked
+            if(isVisited[x,y] || map[x,y] == 'X'){
+                return;
+            } else {
+                // Visit map
+                isVisited[x,y] = true;
+
+                // Add map to track
+                track.Add(new Tuple<int,int>(x,y));
+
+                // If it reached starting point
+                if(map[x,y] == 'K'){
+                    finish = true;
+                    path.AddRange(track);
+                    return;
+                }
+
+                // DFS on L-U-R-D pattern
+                tspDFS(map, x, y-1, x, y, isVisited, path, track, ref finish);
+                tspDFS(map, x-1, y, x, y, isVisited, path, track, ref finish);
+                tspDFS(map, x, y+1, x, y, isVisited, path, track, ref finish);
+                tspDFS(map, x+1, y, x, y, isVisited, path, track, ref finish);
+
+                // If track is blocked and nowhere to continue
+                if(xBefore-1 >= 0 && xBefore+1 < size && yBefore-1 >= 0 && yBefore+1 < size && isVisited[xBefore-1,yBefore] && isVisited[xBefore,yBefore+1] && isVisited[xBefore+1,yBefore] && isVisited[xBefore,yBefore-1]){
+                    track.Clear();
+                } else if (track.Count > 0) {
+                    // Remove current map from track and continue add another map to track
+                    track.RemoveAt(track.Count - 1);
+                }
+                return;
+            }
+        } else {
+            return;
+        }
+    }
+
     static void printMap(char[,] map){
         // Function to print the map
         int size = map.GetLength(0);
@@ -89,6 +145,15 @@ class DFS {
         }
     }
 
+    static void setSolutionTSP(char[,] buffer, List<Tuple<int,int>> path, Tuple<int,int> start, Tuple<int,int> end){
+        // Function to plot the path to starting point
+        foreach (var coor in path){
+            buffer[coor.Item1, coor.Item2] = 'P';
+        }
+        buffer[start.Item1, start.Item2] = 'K';
+        buffer[end.Item1, end.Item2] = 'L';
+    }
+
     static Tuple<int,int> getStartingPoint(char[,] map, int size){
         // Function to get the starting point of the map
         for(int i = 0; i < size; i++){
@@ -105,22 +170,28 @@ class DFS {
         // Main function to test
 
         // N = size of map
-        const int N = 5;
+        const int N = 6;
 
         // Initialize lists
         List<Tuple<int,int>> path = new List<Tuple<int,int>>();
         List<Tuple<int,int>> track = new List<Tuple<int,int>>();
         List<Tuple<int,int>> treasure = new List<Tuple<int,int>>();
-        char[,] map = {{'R', 'R', 'K', 'X', 'T'},
-                       {'R', 'X', 'X', 'X', 'R'},
-                       {'R', 'R', 'R', 'R', 'R'},
-                       {'R', 'X', 'R', 'X', 'R'},
-                       {'X', 'T', 'R', 'X', 'R'}};
+        List<Tuple<int,int>> pathTSP = new List<Tuple<int,int>>();
+        List<Tuple<int,int>> trackTSP = new List<Tuple<int,int>>();
+        char[,] map = {{'T', 'R', 'K', 'R', 'R', 'R'},
+                       {'R', 'X', 'X', 'X', 'X', 'R'},
+                       {'R', 'R', 'X', 'T', 'R', 'R'},
+                       {'X', 'R', 'X', 'X', 'X', 'X'},
+                       {'R', 'R', 'R', 'R', 'R', 'R'},
+                       {'T', 'X', 'X', 'T', 'X', 'T'}};
         char[,] solution = new char[N, N];
         bool[,] isVisited = new bool[N, N];
+        char[,] tspMap = new char[N, N];
+        bool[,] isVisitedTSP = new bool[N, N];
 
         // Set all of the solution map with 'X' char
         memset(solution, 'X', N);
+        memset(tspMap, 'X', N);
         
         // Set all of the isVisited value with false
         for(int i = 0; i < N; i++){
@@ -128,9 +199,15 @@ class DFS {
                 isVisited[i,j] = false;
             }
         }
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                isVisitedTSP[i,j] = false;
+            }
+        }
 
         // Initialize variable to count the treasures
         int count = 0;
+        bool finishTSP = false;
 
         // Start timer
         var stopwatch = new Stopwatch();
@@ -145,6 +222,12 @@ class DFS {
         // Plotting the solution
         setSolution(solution, path, treasure);
 
+        // TSP Algorithm
+        tspDFS(map, treasure.Last().Item1, treasure.Last().Item2, treasure.Last().Item1, treasure.Last().Item2, isVisitedTSP, pathTSP, trackTSP, ref finishTSP);
+
+        // Plotting the TSP
+        setSolutionTSP(tspMap, pathTSP, startingCoor, treasure.Last());
+
         // Stop timer
         stopwatch.Stop();
 
@@ -154,6 +237,9 @@ class DFS {
 
         Console.WriteLine("\nPeta Jalur ke Harta Karun");
         printMap(solution);
+        
+        Console.WriteLine("\nPeta Jalur Kembali ke Starting Point dari Treasure Terakhir");
+        printMap(tspMap);
 
         // Print solution
         Console.WriteLine($"\nTerdapat {count} treasure yang dapat ditemukan.");
