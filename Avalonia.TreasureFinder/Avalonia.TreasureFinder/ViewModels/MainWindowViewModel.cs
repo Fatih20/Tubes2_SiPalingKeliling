fac191b0b9e20978b5f32a54e4b826278831b83b
Fatih20 <fnri39@protonmail.com>
2023-03-20 14:12:31 +0700

    Initial work at reactivity.

--- Avalonia.TreasureFinder/Avalonia.TreasureFinder/ViewModels/MainWindowViewModel.cs
+++ Avalonia.TreasureFinder/Avalonia.TreasureFinder/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System;
 using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
 using Avalonia.TreasureFinder.Models;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ReactiveUI;
@@ -19,8 +21,15 @@ public enum ApplicationState : int
 }
 public class MainWindowViewModel : ViewModelBase
 {
-    // private ApplicationState _state = ApplicationState.FileNotLoaded;
-    private ApplicationState _state = ApplicationState.ShowingResults;
+
+    public MainWindowViewModel()
+    {
+        _inputBarIsOpen = this.WhenAnyValue(x => x).Select(condition => condition.State == ApplicationState.FileNotLoaded).ToProperty(this, x => x.InputBarIsOpen);
+        _inputBarIsHidden = this.WhenAnyValue(x => x.AllBarHidden).Select(condition => condition).ToProperty(this, x => x.InputBarIsHidden);
+    }
+    
+    private ApplicationState _state = ApplicationState.FileNotLoaded;
+    // private ApplicationState _state = ApplicationState.ShowingResults;
 
     private Graph? _graphRepresentation;
 
@@ -69,65 +78,113 @@ public class MainWindowViewModel : ViewModelBase
     }
     // Appearance concerned attributes
     
-    private bool _allBarHidden = true;
+    private bool _allBarHidden;
     public bool AllBarHidden
     {
         get => _allBarHidden;
-        set => this.RaiseAndSetIfChanged(ref _allBarHidden, value);
+        set {
+        this.RaiseAndSetIfChanged(ref _allBarHidden, value);
+        // PropertyChanged(nameof(_allBarHidden));
+
+        }
     }
+    
+    
 
     public void ToggleAllBarHidden()
     {
-        AllBarHidden = true;
+        Console.WriteLine("Toggling");
+        AllBarHidden = !AllBarHidden;
+        // Console.WriteLine("The all bar condition");
+        // Console.WriteLine(AllBarHidden);
+        // Console.WriteLine("The rest of the bar condition");
+        // Console.WriteLine(InputBarIsHidden);
+        // Console.WriteLine(ReplayBarIsHidden);
+        // Console.WriteLine(ResultBarIsHidden);
+        // Console.WriteLine("The rest of the bar open condition");
+        // Console.WriteLine(InputBarIsOpen);
+        // Console.WriteLine(ReplayBarIsOpen);
+        // Console.WriteLine(ResultBarIsOpen);
+        
+        // // Change input bar status
+        // InputBarIsOpen = !AllBarHidden;
+        // InputBarIsHidden = AllBarHidden;
+        //
+        // // Change result bar status
+        // ResultBarIsHidden = AllBarHidden;
+        // ResultBarIsDiscreet = !AllBarHidden;
+        // ResultBarIsOpen = !AllBarHidden;
+        //
+        // // Change replay bar status
+        // ReplayBarIsHidden = AllBarHidden;
+        // ReplayBarIsDiscreet = !AllBarHidden;
+        // ReplayBarIsOpen = AllBarHidden;
+
+        Console.WriteLine("Toggled");
     }
     
-    public bool ShowError
-    {
-        get => ExceptionMessage != null;
-    }
+    readonly ObservableAsPropertyHelper<bool> _inputBarIsOpen;
+    readonly ObservableAsPropertyHelper<bool> _inputBarIsHidden;
+
+    public bool InputBarIsOpen => _inputBarIsOpen.Value;
+    public bool InputBarIsHidden => _inputBarIsHidden.Value;
+
+    // public bool InputBarIsOpen
+    // {
+    //     get => State == ApplicationState.FileNotLoaded && !AllBarHidden;
+    //     private set
+    //     {
+    //     }
+    // }
     
-    // Input Bar State
-    public bool InputBarIsOpen
-    {
-        get => State == ApplicationState.FileNotLoaded && !AllBarHidden;
-    }
-    
-    public bool InputBarIsHidden
-    {
-        get => AllBarHidden;
-    }
-    
+    // public bool InputBarIsHidden
+    // {
+    //     get => AllBarHidden;
+    //     private set {}
+    // }
+    //
     // Result Bar State
     public bool ResultBarIsHidden
     {
         get => AllBarHidden || State == ApplicationState.FileNotLoaded || State == ApplicationState.FileLoading || State == ApplicationState.FileLoaded || State == ApplicationState.CalculatingResults;
+        private set {}
     }
     
     public bool ResultBarIsDiscreet
     {
         get => !ResultBarIsHidden && !ResultBarIsOpen;
+        private set {}
     }
     
     public bool ResultBarIsOpen
     {
         get => State == ApplicationState.PausingRecording || State == ApplicationState.ShowingResults && !AllBarHidden;
+        private set {}
     }
     
     // Replay Bar State
     public bool ReplayBarIsHidden
     {
         get => AllBarHidden || State == ApplicationState.FileNotLoaded || State == ApplicationState.FileLoading || State == ApplicationState.FileLoaded || State == ApplicationState.CalculatingResults;
+        private set {}
     }
     
     
     public bool ReplayBarIsDiscreet
     {
         get => !ReplayBarIsHidden && !ReplayBarIsOpen;
+        private set {}
     }
     
     public bool ReplayBarIsOpen
     {
         get => State == ApplicationState.PausingRecording && !AllBarHidden;
+        private set {}
+    }
+
+    public void ChangeState()
+    {
+        State = ApplicationState.ShowingResults;
     }
 
 
